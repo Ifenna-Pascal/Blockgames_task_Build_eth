9075781a2f56e92cda0eb85dca42dcb3bd3f9915
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-04-03 02:18:54 +0100

    commited

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
+    address owner = 0x441e4635D9D6e53027d6a817F2Aa81fe37F39988;
     constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        _mint(owner, 1000 * 10 ** 18);
     }
 }
